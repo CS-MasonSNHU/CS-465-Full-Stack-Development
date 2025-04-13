c02e433f58eefa38771591d35a8ba8f278a23e51
Mason Williams <mason.williams@snhu.edu>
2025-04-12 21:14:11 -0400

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -20,14 +20,22 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
-app.set('view engine', 'hbs');
 
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
 //wire up controllers
 app.use('/', indexRouter);
