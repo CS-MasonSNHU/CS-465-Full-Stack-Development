8e52598516c9504c278145f556df7d836858f641
Mason Williams <mason.williams@snhu.edu>
2025-04-18 21:57:19 -0400

    Module 7 completed baseline

--- app_api/controller/trips.js
+++ app_api/controller/trips.js
@@ -12,7 +12,7 @@ const tripsList = async(req, res) => {
 
         //Uncomment the following line to show results of query
         //on the console
-        // console.log(q)
+        console.log(q)
 
     if(!q)
     { // Database returned no data
@@ -66,59 +66,53 @@ const tripsAddTrip = async(req, res) => {
         image: req.body.image,
         description: req.body.description
     });
-    
+            
     const q = await newTrip.save();
-        if(!q)
-        { // Database returned no data
+
+        if (!q)
+        {
             return res
                 .status(400)
                 .json(err);
-        } else { //Return new trip
+        } else {
             return res
                 .status(201)
                 .json(q);
         }
-        // Uncomment the following line to show results of operation
-        // on the console
-        // console.log(q);
+            //console.log(q);
 };
 
 // PUT: /trips/:tripCode - Adds a new Trip
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requesting client
 const tripsUpdateTrip = async(req, res) => {
-    // Uncomment for debugging
-    console.log(req.params);
-    console.log(req.body);
+    //console.log(req.params);
+    //console.log(req.body);
     const q = await Model
-        .findOneAndUpdate(
-            { 'code' : req.params.tripCode },
-            {
-                code: req.body.code,
-                name: req.body.name,
-                length: req.body.length,
-                start: req.body.start,
-                resort: req.body.resort,
-                perPerson: req.body.perPerson,
-                image: req.body.image,
-                description: req.body.description
-            }
-        )
+        .findOneAndUpdate({'code': req.params.tripCode },{
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        })
         .exec();
 
-        if(!q)
-        { // Database returned no data
-            return res
-            .status(400)
-            .json(err);
-        } else { // Return resulting updated trip
+            if(!q)
+            {
+                return res 
+                    .status(400)
+                    .json(err);
+            } else {
                 return res
-                .status(201)
-                .json(q);
-        }
-    // Uncomment the following line to show results of operation
-    // on the console
-    // console.log(q);
+                    .status(201)
+                    .json(q);
+            }
+
+                //console.log(q);
 };
 
 module.exports = {
